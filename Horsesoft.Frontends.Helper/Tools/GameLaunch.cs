50959998b789f1d4b42973fc7991a7aef5631d64
dave <dub.cyclone@gmail.com>
2017-08-25 08:46:32 +0100

    add game launch

---
+++ Horsesoft.Frontends.Helper/Tools/GameLaunch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Horsesoft.Frontends.Helper.Tools
+{
+    public class GameLaunch
+    {
+        public void RocketLaunchGame(string RlPath, string systemName, string RomName, string HsPath)
+        {
+            if (Directory.Exists(RlPath))
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start(RlPath + "\\Rocketlauncher.exe",
+                        "-s " + "\"" + systemName + "\"" + " -r " + "\"" + RomName + "\""
+                        + " -f " + HsPath + "\\HyperSpin.exe"
+                        + " -p " + "HyperSpin");
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+
+        public void RocketLaunchGameWithMode(string RlPath,
+            string systemName, string RomName, string mode)
+        {
+            if (Directory.Exists(RlPath))
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start(RlPath +
+                        "\\Rocketlauncher.exe",
+                        "-s " + "\"" + systemName + "\"" + " -r " + "\"" + RomName + "\"" +
+                        //" -f " + HsPath + "\\HyperSpin.exe" +
+                        " -m " + mode + " -p hyperspin");
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+    }
+}
