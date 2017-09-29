e8f5931414ea036bce0ffc3471a8017ba72fe883
dave <dub.cyclone@gmail.com>
2017-09-29 14:57:59 +0100

    Add initial tests to do for system backups.

---
+++ Horsesoft.Frontends.Helper.Tests/IntergrationTests/SystemBackupTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horsesoft.Frontends.Helper.Tests.IntergrationTests
+{
+    public class SystemBackupTests
+    {
+        /*          
+         Databases: Copies whole folder for database.
+         Media: Images, Themes, Video, All                  
+         */
+    }
+}
